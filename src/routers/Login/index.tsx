8a2c311ea2518f8cb1696d5e41cb9164be046c4d
Leonardo Souza <leonardoDevSantos@gmail.com>
2024-07-06 12:28:56 -0300

    Adicionando a verificação com token

--- src/routers/Login/index.tsx
+++ src/routers/Login/index.tsx
@@ -1,12 +1,12 @@
 import { useState } from "react"
-import api from "../../api/api"
-import { useNavigate } from "react-router-dom"
+import api from '../../api/api'
+import { Link, useNavigate } from "react-router-dom"
+import style from './style.module.scss'
 
 
 export default function Login() {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
-    const [error, setError] = useState('')
 
     const navigate = useNavigate()
 
@@ -15,24 +15,45 @@ export default function Login() {
         try {
             const response = await api.post('/login', { email, password })
 
-            const token = sessionStorage.setItem('token', response.data.token)
+            const token = response.data.token
+            sessionStorage.setItem('token', token)
+
             const userId = response.data.user.id
 
             navigate(`/profile/${userId}`)
-
             console.log('Login bem sucedido')
-
         } catch (error) {
             console.log(error)
         }
     }
 
     return<>
-        <form onSubmit={handleLogin}>
-            <input type="email" id="email" placeholder="Digite seu email" value={email} onChange={(e) => setEmail(e.target.value)}/>
-            <input type="password" id="password" placeholder="Digite sua senha" value={password} onChange={(e) => setPassword(e.target.value)}/>
-            <button type="submit">Entrar</button>
-            {error && <p>{error}</p>}
-        </form>
+        <div className={style.container}>
+            <div className={style.arrow}>
+                <Link to='/register'>
+                    <img src="/seta_esquerda.png" alt="arrow_left" className={style.arrow_left}/>
+                </Link>
+            </div>
+            <form onSubmit={handleLogin} className={style.form}>
+
+                <h1 className={style.title}>Digite seus dados</h1>
+
+                <div className={style.divInput}>
+                    {/* email */}
+                    <label className={style.label} htmlFor="email">Email</label>
+                    <input className={style.input} type="email" id="email" placeholder="digite seu email" value={email} onChange={(e) => setEmail(e.target.value)} required/>
+                </div>
+
+                <div className={style.divInput}>
+                    {/* password */}
+                    <label className={style.label} htmlFor="password">Senha</label>
+                    <input className={style.input} type="password" id="password" placeholder="digite sua senha" value={password} onChange={(e) => setPassword(e.target.value)} required/>
+                </div>
+
+                <div>
+                    <button className={style.btn}>Entrar</button>
+                </div>
+            </form>
+        </div>
     </>
 }
